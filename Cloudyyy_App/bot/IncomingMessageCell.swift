9ffbe7dcc24d9906ad5abc534856f5e077ad56fe
joneshlinso <jl9703@srmist.edu.in>
2025-11-10 12:35:46 +0530

    changes

--- Cloudyyy_App/bot/IncomingMessageCell.swift
+++ Cloudyyy_App/bot/IncomingMessageCell.swift
@@ -25,7 +25,7 @@ final class IncomingMessageCell: UITableViewCell {
 
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
         messageLabel.numberOfLines = 0
-        messageLabel.font = UIFont.systemFont(ofSize: 16)
+        messageLabel.font = UIFont(name: "San Francisco", size: 16)
         messageLabel.textColor = .darkText
         bubbleView.addSubview(messageLabel)
 
